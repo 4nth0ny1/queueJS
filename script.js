5d29e6f4b8e82376ebc749ab9436a1c163c8bf14
4nth0ny1 <acatullo4@gmail.com>
2022-02-05 15:09:22 -0600

    added front method to get the first value of the array

--- script.js
+++ script.js
@@ -5,14 +5,28 @@ function Queue () {
     };
 
     this.enqueue = (element) => {
-        collection.push(element);
-    }
+        collection.push(element);   // adds value to the end of the array.  First in, First Out
+    };
+
+    this.dequeue = () => {
+        collection.shift();     // removes the first value of the array. First In, First Out.
+    };
+
+    // additional functions
+    this.front = () => collection[0];
 }
 
 const q = new Queue();
-console.log(q.print()); // -=> []
-q.enqueue(1);           // adds 1 to the queue
-console.log(q.print()); // -=> [1]
+console.log(q.print());                 // -=> []
+q.enqueue(1);                           // adds 1 to the queue
+console.log(q.print());                 // -=> [1]
 
-q.enqueue(2);           // adds 2 to the queue
-console.log(q.print()); // -=> [1,2]
+q.enqueue(2);                           // adds 2 to the queue
+console.log(q.print());                 // -=> [1,2]
+console.log(`Front: ${q.front()}`);     // -=> 2
+
+q.dequeue(2);                           // removes 1 from the queue
+console.log(q.print());                 // -=> [2]
+
+
+
