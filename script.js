65ff6771a2ef132b0eaa540e2ae68000b66fda84
4nth0ny1 <acatullo4@gmail.com>
2022-02-05 15:01:09 -0600

    added enqueue function

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ function Queue () {
         return collection;
         // console.log(collection);
     };
+
+    this.enqueue = (element) => {
+        collection.push(element);
+    }
 }
 
 const q = new Queue();
-console.log(q.print());
+console.log(q.print()); // -=> []
+q.enqueue(1);           // adds 1 to the queue
+console.log(q.print()); // -=> [1]
+
+q.enqueue(2);           // adds 2 to the queue
+console.log(q.print()); // -=> [1,2]
